2395b4105bed1840dd955461dc834eadb32887fe
Yunus Emre Teke <yeteke68@gmail.com>
2022-12-22 01:11:00 +0300

    PRoje oluşturuldu

---
+++ CRM-Area-Replace.DAL/Concreate/EF/Context/CRMContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Area_Replace.DAL.Concreate.EF.Context
+{
+	public class CRMContext : IdentityDbContext
+	{
+
+	}
+}
---
+++ CRM-Area-Replace.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRM_Area_Replace.WebUI.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
